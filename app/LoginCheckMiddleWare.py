602139fce1f42554d2c9d53c05f805c8c662ba26
azamatxabibullayev <azamatxabibullayev02@gmail.com>
2025-02-17 11:24:12 +0500

    added templates, updated views

---
+++ app/LoginCheckMiddleWare.py
@@ -0,0 +1,44 @@
+from django.utils.deprecation import MiddlewareMixin
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+
+class LoginCheckMiddleWare(MiddlewareMixin):
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        modulename = view_func.__module__
+        user = request.user
+
+        if user.is_authenticated:
+            if user.user_type == "1":
+                if modulename == "app.HodViews":
+                    pass
+                elif modulename == "app.views" or modulename == "django.views.static":
+                    pass
+                else:
+                    return redirect("admin_home")
+
+            elif user.user_type == "2":
+                if modulename == "app.StaffViews":
+                    pass
+                elif modulename == "app.views" or modulename == "django.views.static":
+                    pass
+                else:
+                    return redirect("staff_home")
+
+            elif user.user_type == "3":
+                if modulename == "app.StudentViews":
+                    pass
+                elif modulename == "app.views" or modulename == "django.views.static":
+                    pass
+                else:
+                    return redirect("student_home")
+
+            else:
+                return redirect("login")
+
+        else:
+            if request.path == reverse("login") or request.path == reverse("doLogin"):
+                pass
+            else:
+                return redirect("login")
